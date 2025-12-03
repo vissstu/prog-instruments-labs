de526d802909234a059bce08d6d5a1c704979a85
Polina <1vistu1@gmail.ru>
2025-12-03 22:57:01 +0400

    добавила тесты для поиска и выдачи книги

--- lab_6/test_unit.py
+++ lab_6/test_unit.py
@@ -32,3 +32,34 @@ def test_book_return():
     assert book.return_book() == False
 
 
+def test_library_find_book():
+    """Тест поиска книги"""
+    library = Library("Тестовая")
+    library.add_book("Гарри Поттер", "Роулинг")
+
+    # Находим существующую книгу
+    found = library.find_book("Гарри Поттер")
+    assert found is not None
+    assert found.title == "Гарри Поттер"
+
+    # Не находим несуществующую
+    not_found = library.find_book("Несуществующая книга")
+    assert not_found is None
+
+
+def test_library_borrow_book():
+    """Тест выдачи книги"""
+    library = Library("Тестовая")
+    library.add_book("1984", "Оруэлл Дж.")
+
+    # Выдаем книгу
+    assert library.borrow_book("1984", "Иван Иванов") == True
+
+    # Проверяем, что книга теперь выдана
+    book = library.find_book("1984")
+    assert book.is_borrowed == True
+
+    # Пытаемся выдать ту же книгу еще раз
+    assert library.borrow_book("1984", "Петр Петров") == False
+
+
